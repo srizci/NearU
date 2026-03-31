5194d0f8447fa7a340c8783a4ca4e7c9bfc52982
suraizci <izcisura01@gmail.com>
2026-03-31 10:24:37 +0300

    Initial Commit

---
+++ NearU/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NearU
+//
+//  Created by Şura İZCİ on 31.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NearU/NearUApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearUApp.swift
+//  NearU
+//
+//  Created by Şura İZCİ on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct NearUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
